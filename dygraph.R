7b147c23ce07d34a908a33a2ff249ddcf1fbb27c
MahdiHajiabadi <m.hajiabadi67@gmail.com>
2021-08-23 10:29:49 -0700

    Update dygraph.R

--- dygraph.R
+++ dygraph.R
@@ -1,1 +1,16 @@
 # DygraphToPng
+library(dygraphs)
+library(htmlwidgets)
+library(webshot)
+
+#create map
+m <- dygraph(lungDeaths) %>% dyRangeSelector()
+
+# save html to png
+saveWidget(m, "temp.html", selfcontained = FALSE)
+width<- 1080
+height <- 610
+webshot("temp.html", file = "Rplot.png",
+        cliprect = c(10,30,width+50,height+50)
+        ,vwidth = width, vheight = height )
+
